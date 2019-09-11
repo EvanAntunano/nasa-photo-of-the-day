91dde6eef666720d02dcd6d38a865f38d3d2eacb
Evan <evanantunanocoding@gmail.com>
2019-09-11 17:11:10 -0500

    restarted from scratch, didn't notice separate component requirement for MVP. Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+import axios from 'axios';
+import Date from './date';
+import Explanation from './explanation';
+import Media from './media';
+import Title from './title';
+
 function App() {
+  const [space, setSpace] = useState();
+
+  useEffect ( () => { 
+    axios 
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(result => {
+        setSpace(result.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [] );
+
+  let date = null;
+  let explanation = null;
+  let url = null;
+  let title = null;
+
+  if(space) {
+    date = space.date;
+    explanation = space.explanation;
+    url = space.url;
+    title = space.title;
+  }
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+  <div className = "main">
+    <div className = "header">
+    <h1>Nasa Picture of the Day</h1>
+      <Title title = {title} />
+      <Date date = {date} />
+      <Media url = {url} />
+      <Explanation explanation = {explanation} />
     </div>
-  );
+  </div>
+  )
 }
 
-export default App;
+export default App; 
